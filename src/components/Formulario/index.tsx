8adad1b15e2231af05beddad1350ee46a91184c4
Yuri Lima <yurilimmarabelo@gmail.com>
2023-09-25 22:26:40 +0100

    added new components

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Botao from '../Botao';
+
+class Formulario extends React.Component{
+    render(){
+        return(
+            <form>
+                <div>
+                    <label>
+                    Tarefa
+                    </label>
+                    <input type = "text"
+                    name="tarefa"
+                    id="tarefa"
+                    placeholder="O que você quer estudar"
+                    required/>
+                </div>
+                <div>
+                    <label>
+                    Tempo
+                    </label>
+                    <input 
+                    type="time"
+                    step="1"
+                    name="tempo"
+                    id="tempo"
+                    min="00:00:00"
+                    max="01:30:00"
+                    required/>
+
+                </div>
+                <Botao />
+            </form>
+        )
+    }
+}
+
+export default Formulario;
